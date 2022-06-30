d2e496085209fa4e7e77e17c7346253ea9c661d8
Samuel Banya <sbanya@fastmail.com>
2022-06-29 22:16:03 -0400

    Adding latest changes to assignment

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  // NOTE: We received 'onChangeColor' as a prop, which is a function that the
+  // parent, 'Parent' component passed down to the child, 'Child' component
+  console.log("onChangeColor:");
+  console.log(onChangeColor);
+
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
 }
 
 export default Child;
